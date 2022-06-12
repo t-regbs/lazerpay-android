4fb2a9c55aa9da2a492347589ece27704f745249
1Soyebo <soyeboibukun@gmail.com>
2022-06-12 21:40:40 +0100

    fix kotlin lint errors with spacing

--- app/src/main/java/com/timilehinaregbesola/lazerpay/ui/MainActivity.kt
+++ app/src/main/java/com/timilehinaregbesola/lazerpay/ui/MainActivity.kt
@@ -13,11 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.btnLazerpay.setOnClickListener{goToLazerpayWebView()}
+        binding.btnLazerpay.setOnClickListener{ goToLazerpayWebView() }
     }
 
-    private fun goToLazerpayWebView(){
+    private fun goToLazerpayWebView() {
         val i = Intent(applicationContext, LazerpayActivity::class.java)
         startActivity(i)
     }
