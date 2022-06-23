3e8798451d5f690bb433d1e5371b216102c77a4a
Timilehin <aregbestimi@gmail.com>
2022-06-23 12:11:28 +0100

    added Builder to LazerPayCheckout

--- app/src/main/java/com/timilehinaregbesola/lazerpay/LazerPayCheckout.kt
+++ app/src/main/java/com/timilehinaregbesola/lazerpay/LazerPayCheckout.kt
@@ -44,4 +44,66 @@ class LazerPayCheckout private constructor(
             }
         }.launch(lazerpayData)
     }
+
+    class Builder(
+        private val activity: AppCompatActivity,
+        private val publicKey: String,
+        private val name: String,
+        private val email: String,
+        private val amount: String,
+    ) {
+        private var activityResultRegistry: ActivityResultRegistry = activity.activityResultRegistry
+
+        private var businessLogo: String? = null
+        private var currency: LazerPayCurrency = LazerPayCurrency.NGN
+        private var acceptPartialPayment: Boolean = false
+        private var reference: String? = null
+        private var metadata: String? = null
+
+        fun activityResultRegistry(resultRegistry: ActivityResultRegistry): Builder {
+            this.activityResultRegistry = resultRegistry
+            return this
+        }
+
+        fun businessLogo(businessLogo: String): Builder {
+            this.businessLogo = businessLogo
+            return this
+        }
+
+        fun currency(currency: LazerPayCurrency): Builder {
+            this.currency = currency
+            return this
+        }
+
+        fun acceptPartialPayment(acceptPartialPayment: Boolean): Builder {
+            this.acceptPartialPayment = acceptPartialPayment
+            return this
+        }
+
+        fun reference(reference: String): Builder {
+            this.reference = reference
+            return this
+        }
+
+        fun metadata(metadata: String): Builder {
+            this.metadata = metadata
+            return this
+        }
+
+        fun build(): LazerPayCheckout {
+            return LazerPayCheckout(
+                activity,
+                activityResultRegistry,
+                publicKey,
+                name,
+                email,
+                amount,
+                businessLogo,
+                currency,
+                acceptPartialPayment,
+                reference,
+                metadata
+            )
+        }
+    }
 }
